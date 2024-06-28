8b5dd711af806a93ad1081eae63545bd5966ce51
Cyber_Infernox <sayonkar@gmail.com>
2024-06-28 21:32:04 +0530

    Fixes

--- Deployment/React App/PhishGuard/client/src/api/getModelResultAPI.js
+++ Deployment/React App/PhishGuard/client/src/api/getModelResultAPI.js
@@ -14,7 +14,7 @@ const getModelResultAPI = async (url) => {
     console.log("InputJSON" + inputJson);
 
     const response = await axios.post(
-      "https://b798-34-125-214-113.ngrok-free.app", // The public URL provided
+      "https://8cb0-34-125-214-113.ngrok-free.app/url_prediction", // The public URL provided
       inputJson, // Sending JSON as data
       {
         headers: {
